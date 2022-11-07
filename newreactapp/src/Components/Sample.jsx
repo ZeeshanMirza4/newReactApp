31747eb0ca565592decce75a68d52b96623ff509
ZeeshanMirza4 <ZeeshanMirza4>
2022-11-07 13:53:26 +0530

    h1

---
+++ newreactapp/src/Components/Sample.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Sample = () => {
+    return (
+        <div>
+            <h1>hello this is newApp of React</h1>
+        </div>
+    );
+};
+
+export default Sample;
